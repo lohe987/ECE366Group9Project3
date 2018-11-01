00417be4f9cb8dc3e7b1834b2fd9f49b85557f6c
Henry <hhoson2@uic.edu>
2018-11-01 14:01:37 -0500

    Updated Python Simulator

--- Python Simulator/p3_group_9_sim.py
+++ Python Simulator/p3_group_9_sim.py
@@ -16,70 +16,70 @@ def simulate(I,NSteps):
         fetch = I[PC]
         DIC += 1
         print(fetch)
-        if (line[1:8] == "1111100"):
+        if (fetch[1:8] == "1111100"):
             result = Reg[2] ^ Reg[1]    #XorR2R1: R2 = R2 XOR R1
             Reg[2] = result
             PC += 1
 
-    elif (line[1:8] == "1111101"):
-        output_file.write("AndR3                // R3 AND 1 \n")
+        elif (fetch[1:8] == "1111101"):
+            result = Reg[3] & 1     #AndR3: R3 = R3 AND 1
+            Reg[3] = result
+            PC += 1
 
-    elif (line[1:8] == "1111001"):
-        output_file.write("ShiftR               // R2 >> \n")
+        elif (fetch[1:8] == "1111001"):
+            Reg[2] >> 1     #ShiftR: R2 >>
+            PC += 1
 
-    elif (line[1:8] == "1111000"):
-        output_file.write("ShiftL               // R2 << \n")
+        elif (fetch[1:8] == "1111000"):
+            Reg[2] << 1     #ShiftL: R2 <<
+            PC += 1
 
-    elif (line[1:5] == "1110"):
-        register = str(int(line[5:7],2))
-        immediate = str(int(line[7],2))
-        output_file.write("Init R" + register + ", " + immediate)
-        output_file.write("         // R" + register + " = " + immediate + "\n")
+        elif (fetch[1:5] == "1110"):
+            Rx = int(fetch[5:7],2)
+            imm = int(fetch[7],2)
+            Reg[Rx] = imm       #Init: Rx = imm
+            PC += 1
 
-    elif (line[1:4] == "110"):
-        register1 = str(int(line[4:6],2))
-        register2 = str(int(line[6:8],2))
-        output_file.write("SltR1 R" + register1 + ", R" + register2)
-        output_file.write("         // if R" + register1 + " < R" + register2 + ", R1 = 1 \n")
+        elif (fetch[1:4] == "110"):
+            Rx = int(fetch[4:6],2)
+            Ry = int(fetch[6:8],2)
+            if(Reg[Rx] < Reg[Ry]):
+                Reg[1] = 1      #SltR1: if Rx < Ry, R1 = 1
+                PC += 1
+            else:
+                PC +=1
 
-    elif (line[1:4] == "011"):
-        register1 = str(int(line[4:6],2))
-        register2 = str(int(line[6:8],2))
-        output_file.write("Store R" + register1 + ", (R" + register2 + ")")
-        output_file.write("         // M[R" + register2 + "] <- R" + register1 + "\n")
+        elif (fetch[1:4] == "011"):
+            Rx = int(fetch[4:6],2)
+            Ry = int(fetch[6:8],2)
+            Memory[Reg[Ry]] = Reg[Rx]   #Store: M[Ry] <- Rx
+            PC += 1
 
-    elif (line[1:4] == "010"):
-        Rx = (int(line[4:6],2))
-        Ry = (int(line[6:8],2))
-        output_file.write("Load R" + register1 + ", (R" + register2 + ")")
-        output_file.write("         // R" + register1 + " <- M[R" + register2 + "] \n")
+        elif (fetch[1:4] == "010"):
+            Rx = int(fetch[4:6], 2)
+            Ry = int(fetch[6:8], 2)
+            Reg[Rx] = Memory[Reg[Ry]]  # Load: M[Ry] -> Rx
+            PC += 1
 
-    elif (line[1:4] == "001"):          #Rx = Rx - Ry
-        Rx = (int(line[3:4],2)
-        Ry = (int(line[5:6],2)
+        elif (fetch[1:4] == "001"):  # Rx = Rx - Ry
+            Rx = int(fetch[3:4], 2)
+            Ry = int(fetch[5:6], 2)
+            Reg[Rx] = Reg[Rx] - Reg[Ry]
+            PC += 1
 
-        Rx = Rx - Ry
-        
-    elif (fetch[1:4] == "000"):         #Rx = Rx + Ry
-        Rx = int(line[3:4], 2)
-        Ry = int(line[5:6], 2)
-        
-        Rx = Rx + Ry
+        elif (fetch[1:4] == "000"):  # Rx = Rx + Ry
+            Rx = int(fetch[3:4], 2)
+            Ry = int(fetch[5:6], 2)
+            Reg[Rx] = Reg[Rx] + Reg[Ry]
+            PC += 1
 
-    elif (fetch[1:6] == "10000"):       #PC -= Rx
-        Rx = int(line[5:7], 2)
-        PC = PC - Rx
+        elif (fetch[1:6] == "10000"):  # PC -= Rx
+            Rx = int(fetch[5:7], 2)
+            PC = PC - Reg[Rx]
 
-    elif (fetch[1:6] == "10100"):       #If R1==0, PC += Rx
-        Rx = int(line[5:7],2)
-        if (Reg[1] == 0):
-            PC = PC + Rx
-        else:
-            PC = PC + 1
-            
-            
-        
-        
-        register = str(int(line[6:8], 2))
-        output_file.write("bezR1, R" + register)
-        output_file.write("         // if R1 ==0: PC = PC + R" + register + " | else: PC++ \n")
+        elif (fetch[1:6] == "10100"):  # If R1==0, PC += Rx
+            Rx = int(fetch[5:7], 2)
+            if (Reg[1] == 0):
+                PC = PC + Reg[Rx]
+            else:
+                PC += 1
