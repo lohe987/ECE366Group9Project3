b49ad58da0a6a729a456fad888133c1200f812f1
Henry <hhoson2@uic.edu>
2018-11-01 14:53:36 -0500

    Updated Simulator

--- Python Simulator/p3_group_9_sim.py
+++ Python Simulator/p3_group_9_sim.py
@@ -1,13 +1,11 @@
-input_file = open("p3_group_9_p1_mc.txt","r")
-
 print("ECE366 Fall 2018 - Project 3 - Group 9 Python Simulator")
 print()
 
-def simulate(I,NSteps):
+def simulate(I,M):
     PC = 0
     DIC = 0
     Reg = [0,0,0,0]
-    Memory = [0 for i in range(10)]
+    Memory = M
 
     print("###### Start of Simulation ######")
 
@@ -83,3 +81,34 @@ def simulate(I,NSteps):
                 PC = PC + Reg[Rx]
             else:
                 PC += 1
+
+        else:
+            finished = True
+
+    print("******** Simulation finished *********")
+    print("Dynamic Instr Count: ", DIC)
+    print("Registers R0-R3: ", Reg)
+    print("Memory :", Memory)
+
+
+def main():
+    input_file = open("p3_group_9_p1_mc.txt", "r")
+    input_pattern = open("patternA.txt", "r")
+    Nlines = 0  # How many instrs total in input.txt
+    Instruction = []  # all instructions will be stored here
+    Memory = []
+
+    for line in input_file:
+        Instruction.append(line)  # Copy all instruction into a list
+        Nlines += 1
+
+    for line in input_pattern:
+        if(line == "\n"):
+            continue
+        line = line.replace("\n","")
+        Memory.append(int(line,2))
+
+    simulate(Instruction,Memory)
+
+if __name__ == "__main__":
+    main()
